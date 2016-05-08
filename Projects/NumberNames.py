29b8d7317f1db13977c707015a91a2be9e150f30
Tristan Arthur <RGSStudios@outlook.com>
2016-05-08 20:44:01 +1000

    made getting digit length easy

--- Projects/NumberNames.py
+++ Projects/NumberNames.py
@@ -2,11 +2,7 @@ from sys import maxsize
 
 def GetWordFromNum(num):
     simple_change = {0:"zero",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine"}
-    base = 10
-    num_of_digits = 1
-    while num / base > 1:
-        num_of_digits += 1
-        base *= 10
+    num_of_digits = len(str(num))
     if num_of_digits == 1:
         for number_version, text_version in simple_change.items():
             if num == number_version:
